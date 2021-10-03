d9dad47a5a99872e0f9b2364d0ea5c5d48e2ab52
Victor Lopes <thevictorfl@gmail.com>
2021-10-03 18:26:08 -0300

    feature: add create location usecase

---
+++ src/domain/usecase/create-location.ts
@@ -0,0 +1,12 @@
+import Location from "../entity/location";
+import LocationRepository from "../repository/location-repository";
+
+export default class CreateLocation {
+    constructor (readonly locationRepository: LocationRepository) {}
+
+    async execute (name: string, address: string, latitude: number,
+        longitude: number, openHour: number, closeHour: number): Promise<Location> {
+            return this.locationRepository.createLocation(name, address, latitude,
+                longitude, openHour, closeHour)
+    }
+}
---
+++ tests/unit/create-location.spec.ts
@@ -0,0 +1,23 @@
+import LocationRepositoryInMemory from '../../src/infra/repository/location-repository-in-memory'
+import CreateLocation from '../../src/domain/usecase/create-location'
+
+describe('#Location', () => {
+    it('should be able to create a location with valid params', async () => {
+        const locationRepository = new LocationRepositoryInMemory()
+        const createLocation = new CreateLocation(locationRepository)
+
+        const location = await createLocation.execute(
+            'MASP', 'Av. Paulista 123', 1, 1, 8, 18)
+
+        expect(location).toEqual({
+            id: '0',
+            name: 'MASP',
+            address: 'Av. Paulista 123',
+            latitude: 1,
+            longitude: 1,
+            openHour: 8,
+            closeHour: 18,
+            events: []
+        })
+    })
+})
