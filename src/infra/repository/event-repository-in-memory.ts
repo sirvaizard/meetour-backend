4386b07490d0b70fe4d1227cae4306706c7455ad
Victor Lopes <thevictorfl@gmail.com>
2021-10-03 18:25:31 -0300

    feature: add event in memory repository

---
+++ src/infra/repository/event-repository-in-memory.ts
@@ -0,0 +1,17 @@
+import Event from "../../domain/entity/event";
+import Location from "../../domain/entity/location";
+import EventRepository from "../../domain/repository/event-repository";
+
+export default class EventRepositoryInMemory implements EventRepository {
+    readonly events: Event[]
+
+    constructor () {
+        this.events = []
+    }
+
+    createEvent(name: string, description: string, location: Location, begin: Date, capacity: number): Promise<Event> {
+        const event = new Event(String(this.events.length), name, description, location, begin, capacity)
+        return Promise.resolve(event)
+    }
+
+}
