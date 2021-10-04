29723e3648258d9b7fc4f6898015327ddcc6bda2
Victor Lopes <thevictorfl@gmail.com>
2021-10-04 01:43:31 -0300

    fix: should validate before create event

--- src/domain/usecase/create-event.ts
+++ src/domain/usecase/create-event.ts
@@ -9,8 +9,6 @@ export default class CreateEvent {
 
     async execute (name: string, description: string, location: Location,
         begin: Date, capacity: number): Promise<Event> {
-            const event = await this.eventRepository.createEvent(
-                name, description, location, begin, capacity)
 
             if (location.openHour > begin.getHours()) {
                 throw new Error('Cannot create an event before location is opened')
@@ -19,6 +17,9 @@ export default class CreateEvent {
             if (location.closeHour < begin.getHours()) {
                 throw new Error('Cannot create an event after location has been closed')
             }
+
+            const event = await this.eventRepository.createEvent(
+                name, description, location, begin, capacity)
 
             await this.locationRepository.addEvent(location, event)
 
