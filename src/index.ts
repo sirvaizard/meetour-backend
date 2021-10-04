d970a2ed88e328893b30db416adb19c8f6452da4
Victor Lopes <thevictorfl@gmail.com>
2021-10-03 22:26:43 -0300

    feature: add create event route

--- src/index.ts
+++ src/index.ts
@@ -2,17 +2,29 @@ import express from 'express'
 
 import LocationRepositoryInMemory from './infra/repository/location-repository-in-memory'
 import CreateLocationController from './presentation/controller/create-location-controller'
+import CreateEventController from './presentation/controller/create-event-controller'
 import CreateLocation from './domain/usecase/create-location'
+import CreateEvent from './domain/usecase/create-event'
 import ExpressAdapter from './infra/http/express'
+import EventRepositoryInMemory from './infra/repository/event-repository-in-memory'
 
+// Repositories
 const locationRepository = new LocationRepositoryInMemory()
+const eventRepository = new EventRepositoryInMemory()
+
+// UseCases
 const createLocation = new CreateLocation(locationRepository)
+const createEvent = new CreateEvent(eventRepository, locationRepository)
+
+// Controllers
 const createLocationController = new CreateLocationController(createLocation)
+const createEventController = new CreateEventController(createEvent)
 
 const app = express()
     .use(express.json())
 
 app.post('/api/location', ExpressAdapter.create(createLocationController))
+app.post('/api/event/', ExpressAdapter.create(createEventController))
 
 const PORT = process.env.PORT || 3000
 
