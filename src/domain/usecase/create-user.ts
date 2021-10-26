ae6c237a31f76eb857705db5011097cfaa02d2b4
Victor Lopes <thevictorfl@gmail.com>
2021-10-25 23:13:28 -0300

    feature: ensure password is hashed when creating user

--- src/domain/usecase/create-user.ts
+++ src/domain/usecase/create-user.ts
@@ -1,10 +1,13 @@
 import UserRepository from "../repository/user-repository";
+import Hash from "../ports/hash";
 
 export default class CreateUser {
-    constructor (private userRepository: UserRepository) {}
+    constructor (private userRepository: UserRepository, private hash: Hash) {}
 
     async execute (name: string, email: string, password: string, cpf: string, birth: Date) {
-        // TODO: should check for fields validity and hash password
-        return await this.userRepository.create(name, email, password, cpf, birth)
+        // TODO: should check for fields validity
+
+        const password_hash = await this.hash.hash(password)
+        return await this.userRepository.create(name, email, password_hash, cpf, birth)
     }
 }
--- tests/unit/usecase/create-user.spec.ts
+++ tests/unit/usecase/create-user.spec.ts
@@ -1,19 +1,36 @@
 import UserRepositoryInMemory from "../../../src/infra/repository/user-repository-in-memory"
 import UserRepository from "../../../src/domain/repository/user-repository"
 import CreateUser from "../../../src/domain/usecase/create-user"
+import Hash from "../../../src/domain/ports/hash"
+
+const makeHash = (): Hash => {
+    class HashStub implements Hash {
+        hash(text: string): Promise<string> {
+            return Promise.resolve(text)
+        }
+        compare(text: string, hash: string): Promise<boolean> {
+            return Promise.resolve(true)
+        }
+    }
+
+    return new HashStub()
+}
 
 interface SutType {
     userRepository: UserRepository
-    createUser: CreateUser
+    createUser: CreateUser,
+    hash: Hash
 }
 
 const makeSut = (): SutType => {
+    const hash = makeHash()
     const userRepository = new UserRepositoryInMemory
-    const createUser = new CreateUser(userRepository)
+    const createUser = new CreateUser(userRepository, hash)
 
     return {
         userRepository,
-        createUser
+        createUser,
+        hash
     }
 }
 
@@ -33,5 +50,15 @@ describe('#User', () => {
             birth: now,
             cpf: '99999999999'
         })
+    })
+
+    it('should call Hash with correct password', async () => {
+        const { createUser, hash } = makeSut()
+        const hashSpy = jest.spyOn(hash, 'hash')
+
+        await createUser.execute('John Doe', 'john.doe@mail.com',
+            'correctpassword', '99999999999', new Date())
+
+        expect(hashSpy).toHaveBeenCalledWith('correctpassword')
     })
 })
