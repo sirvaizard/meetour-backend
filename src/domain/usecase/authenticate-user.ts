ab532e3cd1f56324ece6883f96ed5fdd6ac8c5e7
Victor Lopes <thevictorfl@gmail.com>
2021-10-25 23:58:07 -0300

    feature: add authenticate user usecase

---
+++ src/domain/usecase/authenticate-user.ts
@@ -0,0 +1,34 @@
+import UserRepository from "../repository/user-repository";
+import Token from "../ports/token";
+import Hash from "../ports/hash";
+
+export default class AuthenticateUser {
+    private APP_SECRET: string
+
+    constructor (private userRepository: UserRepository, private hash: Hash, private token: Token, APP_SECRET: string) {
+        this.APP_SECRET = APP_SECRET
+    }
+
+    async execute (email: string, password: string) {
+        const user = await this.userRepository.findByEmail(email)
+        if (!user) {
+            // TODO: Handle this error better
+            throw new Error()
+        }
+
+        if(!this.hash.compare(password, user.password)) {
+            // TODO: handle this error better
+            throw new Error()
+        }
+
+        if (!this.APP_SECRET) {
+            // TODO: handle this error better
+            throw new Error()
+        }
+        const token = this.token.generate({ id: user.id }, this.APP_SECRET)
+
+        return {
+            token
+        }
+    }
+}
---
+++ tests/unit/usecase/authenticate-user.spec.ts
@@ -0,0 +1,59 @@
+import UserRepositoryInMemory from "../../../src/infra/repository/user-repository-in-memory"
+import AuthenticateUser from "../../../src/domain/usecase/authenticate-user"
+import CreateUser from "../../../src/domain/usecase/create-user"
+import Hash from "../../../src/domain/ports/hash"
+import Token from "../../../src/domain/ports/token"
+
+const makeToken = (): Token => {
+    class TokenStub implements Token {
+        generate (payload: any, secret: string): Promise<string> {
+            return Promise.resolve('tokentokentoken')
+        }
+    }
+
+    return new TokenStub()
+}
+
+const makeHash = (): Hash => {
+    class HashStub implements Hash {
+        hash(text: string): Promise<string> {
+            return Promise.resolve(text)
+        }
+        compare(text: string, hash: string): Promise<boolean> {
+            return Promise.resolve(true)
+        }
+    }
+
+    return new HashStub()
+}
+
+interface SutType {
+    createUser: CreateUser,
+    authenticateUser: AuthenticateUser
+}
+
+const makeSut = (): SutType => {
+    const hash = makeHash()
+    const token = makeToken()
+    const userRepository = new UserRepositoryInMemory()
+    const createUser = new CreateUser(userRepository, hash)
+    const authenticateUser = new AuthenticateUser(userRepository, hash, token, 'secret')
+
+    return {
+        createUser,
+        authenticateUser
+    }
+}
+
+describe('#Authenticate User', () => {
+    it('should athenticate user with valid credentials', async () => {
+        const { createUser, authenticateUser } = makeSut()
+
+        await createUser.execute('John Doe', 'john.doe@mail.com',
+            'correctpassword', '99999999999', new Date())
+
+        const response = await authenticateUser.execute('john.doe@mail.com', 'correctpassword')
+
+        expect(response).toHaveProperty('token')
+    })
+})
