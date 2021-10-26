b89fed02f82a5e93c759fb87afe838a233e2fe89
Victor Lopes <thevictorfl@gmail.com>
2021-10-26 01:46:39 -0300

    feature: add create user endpoint

--- src/index.ts
+++ src/index.ts
@@ -2,32 +2,44 @@ import express from 'express'
 
 import LocationRepositoryInMemory from './infra/repository/location-repository-in-memory'
 import EventRepositoryInMemory from './infra/repository/event-repository-in-memory'
+import UserRepositoryInMemory from './infra/repository/user-repository-in-memory'
 
 import CreateLocationController from './presentation/controller/create-location-controller'
 import CreateEventController from './presentation/controller/create-event-controller'
+import CreateUserController from './presentation/controller/create-user-controller'
 
 import CreateLocation from './domain/usecase/create-location'
 import CreateEvent from './domain/usecase/create-event'
+import CreateUser from './domain/usecase/create-user'
+
+import BcryptHash from './infra/adapters/bcryptjs-hash'
 
 import ExpressAdapter from './infra/http/express'
+
+// Adapters
+const hash = new BcryptHash()
 
 // Repositories
 const locationRepository = new LocationRepositoryInMemory()
 const eventRepository = new EventRepositoryInMemory()
+const userRepository = new UserRepositoryInMemory()
 
 // UseCases
 const createLocation = new CreateLocation(locationRepository)
 const createEvent = new CreateEvent(eventRepository, locationRepository)
+const createUser = new CreateUser(userRepository, hash)
 
 // Controllers
 const createLocationController = new CreateLocationController(createLocation)
 const createEventController = new CreateEventController(createEvent)
+const createUserController = new CreateUserController(createUser)
 
 const app = express()
     .use(express.json())
 
 app.post('/api/location', ExpressAdapter.create(createLocationController))
 app.post('/api/event/', ExpressAdapter.create(createEventController))
+app.post('/api/user/', ExpressAdapter.create(createUserController))
 
 const PORT = process.env.PORT || 3000
 
