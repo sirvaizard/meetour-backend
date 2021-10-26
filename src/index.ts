1d9eee181c4669011df8b475910561860ed19322
Victor Lopes <thevictorfl@gmail.com>
2021-10-26 01:48:55 -0300

    feature: add authenticate user endpoint

--- src/index.ts
+++ src/index.ts
@@ -7,17 +7,21 @@ import UserRepositoryInMemory from './infra/repository/user-repository-in-memory
 import CreateLocationController from './presentation/controller/create-location-controller'
 import CreateEventController from './presentation/controller/create-event-controller'
 import CreateUserController from './presentation/controller/create-user-controller'
+import AuthenticateUserController from './presentation/controller/authenticate-user-controller'
 
 import CreateLocation from './domain/usecase/create-location'
 import CreateEvent from './domain/usecase/create-event'
 import CreateUser from './domain/usecase/create-user'
+import AuthenticateUser from './domain/usecase/authenticate-user'
 
 import BcryptHash from './infra/adapters/bcryptjs-hash'
+import JsonWebToken from './infra/adapters/jsonwebtoken'
 
 import ExpressAdapter from './infra/http/express'
 
 // Adapters
 const hash = new BcryptHash()
+const token = new JsonWebToken()
 
 // Repositories
 const locationRepository = new LocationRepositoryInMemory()
@@ -28,11 +32,13 @@ const userRepository = new UserRepositoryInMemory()
 const createLocation = new CreateLocation(locationRepository)
 const createEvent = new CreateEvent(eventRepository, locationRepository)
 const createUser = new CreateUser(userRepository, hash)
+const authenticateUser = new AuthenticateUser(userRepository, hash, token, process.env.APP_SECRET ?? '')
 
 // Controllers
 const createLocationController = new CreateLocationController(createLocation)
 const createEventController = new CreateEventController(createEvent)
 const createUserController = new CreateUserController(createUser)
+const authenticateUserController = new AuthenticateUserController(authenticateUser)
 
 const app = express()
     .use(express.json())
@@ -40,6 +46,7 @@ const app = express()
 app.post('/api/location', ExpressAdapter.create(createLocationController))
 app.post('/api/event/', ExpressAdapter.create(createEventController))
 app.post('/api/user/', ExpressAdapter.create(createUserController))
+app.post('/api/token/', ExpressAdapter.create(authenticateUserController))
 
 const PORT = process.env.PORT || 3000
 
