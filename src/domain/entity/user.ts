38d497a9433cf9f7455095c602621a3294bbd2fe
Victor Lopes <thevictorfl@gmail.com>
2021-10-25 22:02:00 -0300

    feature: add User entity

---
+++ src/domain/entity/user.ts
@@ -0,0 +1,18 @@
+export default class User {
+    readonly id: string
+	readonly cpf: string
+	readonly password: string
+	readonly name: string
+	readonly birth: Date
+	readonly email: string
+
+    constructor (id: string, cpf: string, password: string, name: string,
+        birth: Date, email: string) {
+            this.id = id
+            this.cpf = cpf
+            this.password = password
+            this.name = name
+            this.birth = birth
+            this.email = email
+    }
+}
