8d875486358fa208ee78f418b0d09e0a532edd1b
Victor Lopes <thevictorfl@gmail.com>
2021-10-25 23:42:15 -0300

    fix: user repository in memory was not saving the created user

--- src/infra/repository/user-repository-in-memory.ts
+++ src/infra/repository/user-repository-in-memory.ts
@@ -9,9 +9,11 @@ export default class UserRepositoryInMemory implements UserRepository {
     }
 
     create(name: string, email: string, password: string, cpf: string, birth: Date): Promise<User> {
-        return Promise.resolve(new User(
+        const user = new User(
             String(this.users.length), cpf, password, name, birth, email
-        ))
+        )
+        this.users.push(user)
+        return Promise.resolve(user)
     }
 
     findById(): Promise<User | null> {
