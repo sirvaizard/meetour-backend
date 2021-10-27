a1382b44b291fe8caed146ee75aea67fe1bc025a
Victor Lopes <thevictorfl@gmail.com>
2021-10-27 20:58:52 -0300

    feature: add headers to express adapter

--- src/infra/http/express.ts
+++ src/infra/http/express.ts
@@ -4,7 +4,7 @@ export default class ExpressAdapter {
     static create (controller: Controller) {
         const handler = controller.execute.bind(controller)
         return async function (req: any, res: any) {
-            const { body, statusCode} = await handler({body: req.body, params: req.params})
+            const { body, statusCode} = await handler({body: req.body, params: req.params, headers: req.headers})
             return res.status(statusCode).json(body)
         }
     }
