d13f9a3b37587fabfc34750d5042b192cf9ae54d
Victor Lopes <thevictorfl@gmail.com>
2021-10-28 22:45:42 -0300

    feature: add addAttendee method to event repository

--- src/domain/repository/event-repository.ts
+++ src/domain/repository/event-repository.ts
@@ -1,7 +1,10 @@
 import Location from "../entity/location";
 import Event from "../entity/event";
+import User from "../entity/user";
 
 export default interface EventRepository {
     createEvent (name: string, description: string, location: Location,
         begin: Date, capacity: number): Promise<Event>
+
+    addAttendee (event: Event, user: User): Promise<boolean>
 }
