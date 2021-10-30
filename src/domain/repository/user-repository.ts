e98e3cc075bff8e9cc2927cf5b64e1c80f6a6ecf
Victor Lopes <thevictorfl@gmail.com>
2021-10-29 21:29:56 -0300

    feature: allow user repositories to return undefined

--- src/domain/repository/user-repository.ts
+++ src/domain/repository/user-repository.ts
@@ -2,7 +2,7 @@ import User from "../entity/user";
 
 export default interface UserRepository {
     create (name: string, email: string, password: string, cpf: string, birth: Date): Promise<User>
-    findById (id: string): Promise<User | null>
-    findByCpf (cpf: string): Promise<User | null>
-    findByEmail (email: string): Promise<User | null>
+    findById (id: string): Promise<User | null | undefined>
+    findByCpf (cpf: string): Promise<User | null | undefined >
+    findByEmail (email: string): Promise<User | null | undefined>
 }
