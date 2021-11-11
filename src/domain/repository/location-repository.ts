56739407cfd827ff6f0b987eeba209563c634d38
Victor Lopes <thevictorfl@gmail.com>
2021-11-10 21:29:02 -0300

    refactor: includes null to getLocation method return types

--- src/domain/repository/location-repository.ts
+++ src/domain/repository/location-repository.ts
@@ -3,6 +3,6 @@ import Event from '../entity/event'
 
 export default interface LocationRepository {
     createLocation (name: string, address: string, latitude: number, longitude: number, openHour: number, closeHour: number): Promise<Location>
-    getLocation (id: string): Promise<Location>
+    getLocation (id: string): Promise<Location | null>
     addEvent (location: Location, event: Event): Promise<Event[]>
 }
