77b2574a5ae3571d6e1d08d31c7cd7d688e654bf
Victor Lopes <thevictorfl@gmail.com>
2021-11-17 19:13:23 -0300

    refactor: remove spaces and unused import

--- src/domain/usecase/create-event.ts
+++ src/domain/usecase/create-event.ts
@@ -1,6 +1,5 @@
 import LocationRepository from '../repository/location-repository'
 import EventRepository from '../repository/event-repository'
-import Location from '../entity/location'
 import Event from '../entity/event'
 
 export default class CreateEvent {
@@ -9,13 +8,11 @@ export default class CreateEvent {
 
     async execute (name: string, description: string, location: string,
         begin: Date, capacity: number): Promise<Event> {
-
             const locationExists = await this.locationRepository.getLocation(location)
 
             if (!locationExists) {
                 throw new Error('Cannot create an event in a location that does not exists')
             }
-
             if (locationExists.openHour > begin.getHours()) {
                 throw new Error('Cannot create an event before location is opened')
             }
